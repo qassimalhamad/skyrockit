e042e6ab3c2c4c189c6772b13c3a346b58df6b86
Qasim <qalhamad33@gmail.com>
2024-07-24 14:36:01 +0300

    bulding and updating the home page

--- models/user.js
+++ models/user.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose');
-
+const applicationSchema = mongoose.Schema({
+  company: String,
+  title: String,
+  date: String,
+  notes: String,
+  postingLink: String,
+  status: {
+    enum: [ 'interested' , 'applied' , 'interviewwing' , 'rejected' , 'accepted' ] 
+  }
+},{timestamps: true})
 const userSchema = mongoose.Schema({
   username: {
     type: String,
@@ -9,6 +18,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  application: [applicationSchema] //embeded
 });
 
 const User = mongoose.model('User', userSchema);
